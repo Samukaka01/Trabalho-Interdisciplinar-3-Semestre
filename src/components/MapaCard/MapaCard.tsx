6fbbad4711dd5b84ec01bf8665106b54af73fb89
Samukaka01 <samuca.rib0109@gmail.com>
2025-07-30 18:43:24 -0300

    mudancas de estilização

--- src/components/MapaCard/MapaCard.tsx
+++ src/components/MapaCard/MapaCard.tsx
@@ -1,6 +1,5 @@
-// src/components/MapaCard/MapaCard.tsx
 import React from 'react';
-import styles from './MapaCard.module.css'; // Mantenha para estilos específicos de MapaCard
+import styles from './MapaCard.module.css'; 
 import CardGenerico from '../CardGenerico/CardGenerico';
 import { IMapa } from '../../Interfaces/IMapa';
 
@@ -11,11 +10,11 @@ interface Props {
 const MapaCard: React.FC<Props> = ({ mapa }) => {
   return (
     <CardGenerico
-      imageUrl={mapa.listViewIcon || mapa.displayIcon} // Use uma das duas opções de ícone
+      imageUrl={mapa.listViewIcon || mapa.displayIcon} 
       title={mapa.displayName}
-      // O onClick será gerenciado pelo ListaMapas
-      cardClassName={styles.mapaCardCustom} // Opcional: para estilos específicos deste card
-      imageClassName={styles.mapaCardImage} // Opcional: para estilos específicos da imagem
+      
+      cardClassName={styles.mapaCardCustom} 
+      imageClassName={styles.mapaCardImage} 
     />
   );
 };
